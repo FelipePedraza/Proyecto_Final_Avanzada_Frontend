9e310eac27e66bf95474133c7148e663b58fd7c4
FelipePedraza <davidf.pedrazab@uqvirtual.edu.co>
2025-11-06 17:53:55 -0500

    se crea el componente dahsboard-anfitiron

---
+++ src/app/pages/dashboard-anfitrion/dashboard-anfitrion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-anfitrion',
+  imports: [],
+  templateUrl: './dashboard-anfitrion.html',
+  styleUrl: './dashboard-anfitrion.css'
+})
+export class DashboardAnfitrion {
+
+}
